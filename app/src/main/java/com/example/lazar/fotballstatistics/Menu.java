0c52fcaf2a1d94e3f41294d0e5ce460a33530650
art-tec <etini.artur@outlook.com>
2019-11-10 17:01:41 -0600

    Version con interfazes funcionales

---
+++ app/src/main/java/com/example/lazar/fotballstatistics/Menu.java
@@ -0,0 +1,27 @@
+package com.example.lazar.fotballstatistics;
+
+import android.content.Intent;
+import android.support.constraint.ConstraintLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class Menu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+
+        ConstraintLayout cl = (ConstraintLayout) findViewById(R.id.partido1);
+        cl.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent in = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(in);
+
+            }
+        });
+
+    }
+}
